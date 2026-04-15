bf0a211caccba2b0ad395befb6251d658845a218
Milan Gauswami <gauswamimilan17@gmail.com>
2026-04-15 16:27:38 +0530

    bit manipulation

--- Bit_MANIPULATION/checkOddEven.java
+++ Bit_MANIPULATION/checkOddEven.java
@@ -26,9 +26,35 @@ public class checkOddEven {
         }
     }
 
+    public static int setIthBit(int n, int i) {
+        int bitMask = 1<<i;
+        return n | bitMask;
+    }
+
+    public static int clearIthBit(int n, int i) {
+        int bitMask = 1<<i;
+        return (n & (~bitMask));
+    }
+
+    public static int updateIthBit(int n, int i, int newBit) {
+//        if(newBit == 0){
+//            return clearIthBit(n, i);
+//        }
+//        else {
+//            return setIthBit(n, i);
+//        }
+
+        n = clearIthBit(n,i);
+        int bitMask = newBit<<i;
+        return (n | bitMask);
+    }
+
     static void main() {
 //        OddEven(10);
 //        OddEven(11);
-        System.out.println(getIthBit(10,2));
+//        System.out.println(getIthBit(10,2));
+//        System.out.println(setIthBit(10,2));
+//        System.out.println(clearIthBit(10,1));
+        System.out.println(updateIthBit(10,2,1));
     }
 }
